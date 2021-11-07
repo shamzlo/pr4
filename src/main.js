2d90abbc2cbf7cdda44626225649780bbc1b879d
shamzlo <93603923+shamzlo@users.noreply.github.com>
2021-11-07 22:56:47 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './components/App.vue'
+import Students from './components/Students.vue'
+import StudentInfo from './components/StudentInfo.vue'
+
+const routes = [
+    {path: '/', component: Students},
+    {path: '/student-info/:id', component: StudentInfo,props:true},
+]
+
+const router = new VueRouter({
+    routes
+})
+
+    Vue.use(VueAxios, axios)
+    Vue.use(VueRouter)
+
+
+new Vue({
+   render: h => h(App),
+   el: '#app',
+   router
+}) 
